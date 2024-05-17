2824700ee90d0c5544d58a82cebb7b63823da3c5
Nangmanful <154849613+Nangmanful@users.noreply.github.com>
2024-05-17 21:32:39 +0900

    Update linetracer.c

--- linetracer.c
+++ linetracer.c
@@ -2,67 +2,51 @@
 #include <wiringPi.h>
 
 // 4개의 트래킹 핀 정의 (WiringPi 핀 번호, 실제 GPIO 핀 번호는 주석으로 표시)
-#define TRACKING_PIN1 7  // WiringPi pin 0, corresponds to GPIO 17
-#define TRACKING_PIN2 0  // WiringPi pin 1, corresponds to GPIO 18
-#define TRACKING_PIN3 3  // WiringPi pin 2, corresponds to GPIO 27
-#define TRACKING_PIN4 2  // WiringPi pin 3, corresponds to GPIO 22
+#define TRACKING_PIN1 7  
+#define TRACKING_PIN2 0  
+#define TRACKING_PIN3 3  
+#define TRACKING_PIN4 2  
 
-#define MOTOR_LEFT_FORWARD 4    // WiringPi pin 4, corresponds to GPIO 23
-#define MOTOR_LEFT_BACKWARD 5   // WiringPi pin 5, corresponds to GPIO 24
-#define MOTOR_RIGHT_FORWARD 6   // WiringPi pin 6, corresponds to GPIO 25
-#define MOTOR_RIGHT_BACKWARD 8  // WiringPi pin 7, corresponds to GPIO 4
+#define MOTOR_RIGHT 8   
+#define MOTOR_LEFT 9
 
 // 자동차 초기화 함수
 void car_init() {
-    pinMode(MOTOR_LEFT_FORWARD, OUTPUT);
-    pinMode(MOTOR_LEFT_BACKWARD, OUTPUT);
-    pinMode(MOTOR_RIGHT_FORWARD, OUTPUT);
-    pinMode(MOTOR_RIGHT_BACKWARD, OUTPUT);
+    pinMode(MOTOR_LEFT, OUTPUT);
+    pinMode(MOTOR_RIGHT, OUTPUT);
 
-    digitalWrite(MOTOR_LEFT_FORWARD, LOW);
-    digitalWrite(MOTOR_LEFT_BACKWARD, LOW);
-    digitalWrite(MOTOR_RIGHT_FORWARD, LOW);
-    digitalWrite(MOTOR_RIGHT_BACKWARD, LOW);
+    digitalWrite(MOTOR_LEFT, LOW);
+    digitalWrite(MOTOR_RIGHT, LOW);
 }
 
 // 전진 함수
 void forward() {
-    digitalWrite(MOTOR_LEFT_FORWARD, HIGH);
-    digitalWrite(MOTOR_LEFT_BACKWARD, LOW);
-    digitalWrite(MOTOR_RIGHT_FORWARD, HIGH);
-    digitalWrite(MOTOR_RIGHT_BACKWARD, LOW);
+    digitalWrite(MOTOR_LEFT, HIGH);
+    digitalWrite(MOTOR_RIGHT, HIGH);
 }
 
 // 후진 함수
 void backward() {
-    digitalWrite(MOTOR_LEFT_FORWARD, LOW);
-    digitalWrite(MOTOR_LEFT_BACKWARD, HIGH);
-    digitalWrite(MOTOR_RIGHT_FORWARD, LOW);
-    digitalWrite(MOTOR_RIGHT_BACKWARD, HIGH);
+    digitalWrite(MOTOR_LEFT, LOW);
+    digitalWrite(MOTOR_RIGHT, LOW);
 }
 
 // 좌회전 함수
 void left() {
-    digitalWrite(MOTOR_LEFT_FORWARD, LOW);
-    digitalWrite(MOTOR_LEFT_BACKWARD, HIGH);
-    digitalWrite(MOTOR_RIGHT_FORWARD, HIGH);
-    digitalWrite(MOTOR_RIGHT_BACKWARD, LOW);
+    digitalWrite(MOTOR_LEFT, HIGH);
+    digitalWrite(MOTOR_RIGHT, LOW);
 }
 
 // 우회전 함수
 void right() {
-    digitalWrite(MOTOR_LEFT_FORWARD, HIGH);
-    digitalWrite(MOTOR_LEFT_BACKWARD, LOW);
-    digitalWrite(MOTOR_RIGHT_FORWARD, LOW);
-    digitalWrite(MOTOR_RIGHT_BACKWARD, HIGH);
+    digitalWrite(MOTOR_LEFT, LOW);
+    digitalWrite(MOTOR_RIGHT, HIGH);
 }
 
 // 정지 함수
 void stop() {
-    digitalWrite(MOTOR_LEFT_FORWARD, LOW);
-    digitalWrite(MOTOR_LEFT_BACKWARD, LOW);
-    digitalWrite(MOTOR_RIGHT_FORWARD, LOW);
-    digitalWrite(MOTOR_RIGHT_BACKWARD, LOW);
+    digitalWrite(MOTOR_LEFT, LOW);
+    digitalWrite(MOTOR_RIGHT, LOW);
 }
 
 // 회전 함수 (각도)
@@ -104,15 +88,19 @@ int main() {
 
         // 트래킹 핀 값을 바탕으로 자동차 제어 로직
         if (trackValue2 == 0 && trackValue3 == 0) {
-            // forward();
+            forward();
             printf("Forward: TrackValue1=%d TrackValue2=%d TrackValue3=%d TrackValue4=%d\n", trackValue1, trackValue2, trackValue3, trackValue4);
         } 
-        else if(trackValue2 == 0 && trackValue3 ==1){
-            // right();
+        else if(trackValue2 == 0 && trackValue3 == 1){
+            right();
+            printf("Forward: TrackValue1=%d TrackValue2=%d TrackValue3=%d TrackValue4=%d\n", trackValue1, trackValue2, trackValue3, trackValue4);
+        }
+        else if(trackValue2 == 1 && trackValue3 == 0){
+            left();
             printf("Forward: TrackValue1=%d TrackValue2=%d TrackValue3=%d TrackValue4=%d\n", trackValue1, trackValue2, trackValue3, trackValue4);
         }
         else {
-            // stop();
+            stop();
             printf("Stop: TrackValue1=%d TrackValue2=%d TrackValue3=%d TrackValue4=%d\n", trackValue1, trackValue2, trackValue3, trackValue4);
         }
 
