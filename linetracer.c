91be3727fd098515dabf5919cdd4ed9020c95029
Nangmanful <154849613+Nangmanful@users.noreply.github.com>
2024-05-17 20:24:38 +0900

    Update linetracer.c

--- linetracer.c
+++ linetracer.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-// 핀 정의
-#define TRACKING_PIN 0  // WiringPi pin 0, corresponds to GPIO 17
-#define MOTOR_PIN 1     // WiringPi pin 1, corresponds to GPIO 18
+// 4개의 트래킹 핀 정의 (WiringPi 핀 번호, 실제 GPIO 핀 번호는 주석으로 표시)
+#define TRACKING_PIN1 0  // WiringPi pin 0, corresponds to GPIO 17
+#define TRACKING_PIN2 1  // WiringPi pin 1, corresponds to GPIO 18
+#define TRACKING_PIN3 2  // WiringPi pin 2, corresponds to GPIO 27
+#define TRACKING_PIN4 3  // WiringPi pin 3, corresponds to GPIO 22
+#define MOTOR_PIN 1      // Motor control pin, WiringPi pin 1, corresponds to GPIO 18
 
 // 자동차 구조체 정의
 typedef struct {
@@ -39,22 +42,34 @@ int main() {
         return 1;
     }
 
-    // 핀 설정
-    pinMode(TRACKING_PIN, INPUT);
+    // 4개의 트래킹 핀을 입력으로 설정
+    pinMode(TRACKING_PIN1, INPUT);
+    pinMode(TRACKING_PIN2, INPUT);
+    pinMode(TRACKING_PIN3, INPUT);
+    pinMode(TRACKING_PIN4, INPUT);
 
     // 자동차 구조체 초기화
     Car car;
     car_init(&car, MOTOR_PIN);
 
     while (1) {
-        int trackValue = digitalRead(TRACKING_PIN);
-        printf("Track Value: %d\n", trackValue);
+        int trackValue1 = digitalRead(TRACKING_PIN1);
+        int trackValue2 = digitalRead(TRACKING_PIN2);
+        int trackValue3 = digitalRead(TRACKING_PIN3);
+        int trackValue4 = digitalRead(TRACKING_PIN4);
 
-        if (trackValue == 0) {
+        // 각 트래킹 핀의 값을 출력
+        printf("Track Values: %d %d %d %d\n", trackValue1, trackValue2, trackValue3, trackValue4);
+
+        // 트래킹 핀 값을 바탕으로 자동차 제어 로직
+        if (trackValue1 == 0 || trackValue2 == 0 || trackValue3 == 0 || trackValue4 == 0) {
             car_forward(&car);
+            printf("Forward: TrackValue1=%d TrackValue2=%d TrackValue3=%d TrackValue4=%d\n", trackValue1, trackValue2, trackValue3, trackValue4);
         } else {
             car_stop(&car);
+            printf("Stop: TrackValue1=%d TrackValue2=%d TrackValue3=%d TrackValue4=%d\n", trackValue1, trackValue2, trackValue3, trackValue4);
         }
+
         delay(100); // 0.1초 대기
     }
 
