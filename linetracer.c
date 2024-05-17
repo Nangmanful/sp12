e95d373b0337a838ad12442c129d0ee9a9b47edf
Nangmanful <154849613+Nangmanful@users.noreply.github.com>
2024-05-18 00:54:08 +0900

    Update linetracer.c

--- linetracer.c
+++ linetracer.c
@@ -100,17 +100,17 @@ int main() {
         // 트래킹 핀 값을 바탕으로 자동차 제어 로직
         if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 1) {
             n = 0;
-            Car_Run(50, 50);
+            Car_Run(40, 40);
             delay(20);
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 1) {
             n = 0;
-            Car_Run(50, 50);
+            Car_Run(40, 40);
             delay(20);
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 0) {
             n = 0;
-            Car_Run(50, 50);
+            Car_Run(40, 40);
             delay(20);
         }
         else if (trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 1 && trackValue4 == 0) {
@@ -133,13 +133,21 @@ int main() {
         }
         else if (trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 0) {
             n = 0;
+            Car_Left(100, 100);
+            delay(100);
             Car_Run(50, 50);
-            delay(20);
+            delay(50);
+            Car_Left(100, 100);
+            delay(100);
         } 
         else if (trackValue2 == 1 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 0) {
             n = 0;
+            Car_Right(100, 100);
+            delay(100);
             Car_Run(50, 50);
-            delay(20);
+            delay(50);
+            Car_Right(100, 100);
+            delay(100);
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 0) {
             n = 0;
@@ -194,7 +202,7 @@ int main() {
         }
         else{
             n = 0;
-            Car_Back(40, 40);
+            Car_Back(30, 30);
             delay(10);
         } 
         delay(10);
