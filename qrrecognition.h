c43f6f08631d6acced7ec47d547af2b5d5547fdb
Nangmanful <154849613+Nangmanful@users.noreply.github.com>
2024-05-30 21:32:28 +0900

    Create qrrecognition.h

---
+++ qrrecognition.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+#include <iostream>
+#include <string>
+
+#include <ctime>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int qrrecogntion();
+
+#ifdef __cplusplus
+}
+#endif
