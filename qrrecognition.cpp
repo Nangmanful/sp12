6ce9d3cbfe155f2e4d15ac3aed76348dbf2c1228
jaehoon-L <151628298+jaehoon-L@users.noreply.github.com>
2024-05-31 00:32:46 +0900

    Update qrrecognition.cpp

--- qrrecognition.cpp
+++ qrrecognition.cpp
@@ -22,12 +22,14 @@ const char* qrrecognition() {
     std::clock_t start_time = std::clock();
 
     while (true) {
+        std::cout << "카메라 시작" << std::endl;
         // 현재 시간을 가져옵니다.
         std::clock_t current_time = std::clock();
 
         // 경과된 시간이 1초 이상인지 확인합니다.
         double elapsed_time = static_cast<double>(current_time - start_time) / CLOCKS_PER_SEC;
         if (elapsed_time >= 1.0) {
+            std::cout << "1초 지남" << std::endl;
             break;
         }
 
@@ -38,6 +40,7 @@ const char* qrrecognition() {
         std::string data = qrDecoder.detectAndDecode(frame, points);
         
         if (!data.empty()) {
+            std::cout << "qr 인식!" << std::endl;
             const char* data2 = data.c_str();;
             return data2;
         }
@@ -45,10 +48,12 @@ const char* qrrecognition() {
         char c = (char)cv::waitKey(25);
         if (c == 27) // ESC 키
             break;
+        std::cout << "카메라 작동 중" << std::endl;
     }
 
     cap.release();
     cv::destroyAllWindows();
+    std::cout << "이제 나감" << std::endl;
     return "77";
 }
 
