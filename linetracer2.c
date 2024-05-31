65e0a012a8eb747e035d26102d466e9e8e9f96bc
Nangmanful <154849613+Nangmanful@users.noreply.github.com>
2024-05-31 21:13:15 +0900

    Update linetracer2.c

--- linetracer2.c
+++ linetracer2.c
@@ -120,11 +120,11 @@ int main() {
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 1) {
             n = 0;
-            Car_Left(40, 40);
+            Car_Left(80, 80);
         }
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 0) {
             n = 0;
-            Car_Right(40, 40);
+            Car_Run(80, 80);
         }
         else if (trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 1 && trackValue4 == 0) {
             n = 0;
@@ -168,7 +168,7 @@ int main() {
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 0) {
             n = 0;
-            Car_Run(40, 40);
+            Car_Left(40, 40);
         } 
         else if(trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 1){
             n = 0;
