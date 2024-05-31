8dc1acb3dc1104edac0c560f1a61f968dcfb762c
JEEEUNN <lobella2647@gmail.com>
2024-06-01 01:35:50 +0900

    Update linetracerJR2.c

--- linetracerJR2.c
+++ linetracerJR2.c
@@ -116,7 +116,7 @@ int main() {
         }
         if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 1) {
             n = 0;
-            Car_Run(40, 40);
+            Car_Run(20, 20);
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 1) {
             n = 0;
@@ -130,7 +130,7 @@ int main() {
         }
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 0) {
             n = 0;
-            Car_Run(40, 40);
+            Car_Run(20, 20);
         }
         else if (trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 1 && trackValue4 == 0) {
             n = 0;
@@ -174,7 +174,7 @@ int main() {
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 0) {
             n = 0;
-            Car_Run(40, 40);
+            Car_Run(20, 20);
         } 
         else if(trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 1){
             n = 0;
