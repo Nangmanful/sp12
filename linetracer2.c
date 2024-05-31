b0807c2b7c8b5451784865ffd462adc70978a0a2
Nangmanful <154849613+Nangmanful@users.noreply.github.com>
2024-05-31 22:27:36 +0900

    Update linetracer2.c

--- linetracer2.c
+++ linetracer2.c
@@ -7,6 +7,7 @@
 #include <linux/i2c-dev.h>
 #include <math.h>
 #include <errno.h>
+#include <stdbool.h> // bool 타입을 사용하기 위해 추가
 
 #define TRACKING_PIN1 7  
 #define TRACKING_PIN2 0  
@@ -179,13 +180,11 @@ int main() {
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 0) {
             n = 0;
-            k = 0;
-            Car_Left(100, 100);
-            delay(80);
-            Car_Run(50, 50);
-            delay(30);
-            Car_Left(100, 100);
-            delay(80);
+            k = 0;            
+            printf("what\n");
+            Car_Left(80, 80);
+            delay(1000);
+            printf("the\n");
         } 
         else if(trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 1){
             n = 0;
