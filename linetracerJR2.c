c5d8bd1494de5df59dfd1f35e3d996da345929cb
JEEEUNN <lobella2647@gmail.com>
2024-06-01 02:05:28 +0900

    Update linetracerJR2.c

--- linetracerJR2.c
+++ linetracerJR2.c
@@ -125,10 +125,6 @@ int main() {
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 1) {
             n = 0;
             k = 0;
-            Car_Run(50, 50);
-            delay(30);
-            Car_Stop();
-            delay(30);
             Car_Right(100, 100);
             delay(80);
             Car_Run(50, 50);
