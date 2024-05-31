21b73a470e55b845dc92c19d9470d17838286cea
JEEEUNN <lobella2647@gmail.com>
2024-06-01 03:01:11 +0900

    Update linetracerJB1.c

--- linetracerJB1.c
+++ linetracerJB1.c
@@ -129,41 +129,41 @@ int main() {
         else if (trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 1 && trackValue4 == 0) {
             n = 0;
             k = 0;
-            Car_Right(100, 100);
+            Car_Left(100, 100);
             delay(80);
             Car_Back(50, 50);
             delay(30);
-            Car_Right(100, 100);
+            Car_Left(100, 100);
             delay(80);
         }
         else if (trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 1 && trackValue4 == 1) {
             n = 0;
             k = 0;
-            Car_Right(100, 100);
+            Car_Left(100, 100);
             delay(80);
             Car_Back(50, 50);
             delay(30);
-            Car_Right(100, 100);
+            Car_Left(100, 100);
             delay(80);
         }
         else if (trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 0) {
             n = 0;
             k = 0;
-            Car_Right(100, 100);
+            Car_Left(100, 100);
             delay(80);
             Car_Back(50, 50);
             delay(30);
-            Car_Right(100, 100);
+            Car_Left(100, 100);
             delay(80);
         } 
         else if (trackValue2 == 1 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 0) {
             n = 0;
             k = 0;
-            Car_Left(100, 100);
+            Car_Right(100, 100);
             delay(80);
             Car_Back(50, 50);
             delay(30);
-            Car_Left(100, 100);
+            Car_Right(100, 100);
             delay(80);
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 0) {
@@ -176,41 +176,41 @@ int main() {
         else if(trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 1){
             n = 0;
             k = 0;
-            Car_Right(100, 100);
+            Car_Left(100, 100);
             delay(80);
             Car_Back(50, 50);
             delay(30);
-            Car_Right(100, 100);
+            Car_Left(100, 100);
             delay(80);
         }
         else if(trackValue2 == 1 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 0){
             n = 0;
             k = 0;
-            Car_Left(100, 100);
+            Car_Right(100, 100);
             delay(80);
             Car_Back(50, 50);
             delay(30);
-            Car_Left(100, 100);
+            Car_Right(100, 100);
             delay(80);
         }
         else if(trackValue2 == 1 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 1){
             n = 0;
             k = 0;
-            Car_Left(100, 100);
+            Car_Right(100, 100);
             delay(80);
             Car_Back(50, 50);
             delay(30);
-            Car_Left(100, 100);
+            Car_Right(100, 100);
             delay(80);
         }
         else if(trackValue2 == 1 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 1){
             n = 0;
             k = 0;
-            Car_Left(100, 100);
+            Car_Right(100, 100);
             delay(80);
             Car_Back(50, 50);
             delay(30);
-            Car_Left(100, 100);
+            Car_Right(100, 100);
             delay(80);
         }
         else if(trackValue2 == 1 && trackValue3 == 1 && trackValue1 == 1 && trackValue4 == 1){
