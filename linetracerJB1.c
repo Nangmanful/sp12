cc3889c446eb0b6f4eca935e3abb9e68394c0adf
JEEEUNN <lobella2647@gmail.com>
2024-06-02 06:09:44 +0900

    Update linetracerJB1.c

--- linetracerJB1.c
+++ linetracerJB1.c
@@ -116,11 +116,11 @@ int main() {
         }
         if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 1) {
             n = 0;
-            Car_Run(70, 70);
+            Car_Run(60, 60);
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 1) {
             n = 0;
-            Car_Run(70, 70);
+            Car_Run(60, 60);
         }
         else if (trackValue2 == 1 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 1) {
             n = 0;
@@ -142,7 +142,7 @@ int main() {
         }
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 0) {
             n = 0;
-            Car_Run(70, 70);
+            Car_Run(60, 60);
         }
         else if (trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 1 && trackValue4 == 0) {
             n = 0;
@@ -166,13 +166,13 @@ int main() {
             Car_Right(200, 200);
             delay(200);
             Car_Left(100, 100);
-            delay(1600);
+            delay(1500);
             Car_Run(40, 40);
-            delay(300);
+            delay(400);
             Car_Right(200, 200);
-            delay(200);
+            delay(300);
             Car_Run(40, 40);
-            delay(200);
+            delay(400);
         } 
         else if(trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 1){
             n = 0;
