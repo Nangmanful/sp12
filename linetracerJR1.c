b12e5314a2cce3900c660e89ca5c156e0b43010e
JEEEUNN <lobella2647@gmail.com>
2024-06-02 00:09:32 +0900

    Update linetracerJR1.c

--- linetracerJR1.c
+++ linetracerJR1.c
@@ -142,8 +142,10 @@ int main() {
         }
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 0) {
             n = 0;
-            Car_Right(100, 100);
-            delay(80);
+            Car_Right(150, 150);
+            delay(150);
+            Car_Back(40, 40);
+            delay(50);
         }
         else if (trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 1 && trackValue4 == 0) {
             n = 0;
@@ -164,8 +166,10 @@ int main() {
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 0) {
             n = 0;
-            Car_Right(100, 100);
-            delay(80);
+            Car_Right(150, 150);
+            delay(150);
+            Car_Back(40, 40);
+            delay(50);
         } 
         else if(trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 1){
             n = 0;
@@ -174,8 +178,10 @@ int main() {
         }
         else if(trackValue2 == 1 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 0){
             n = 0;
-            Car_Right(100, 100);
-            delay(80);
+            Car_Right(150, 150);
+            delay(150);
+            Car_Back(40, 40);
+            delay(50);
         }
         else if(trackValue2 == 1 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 1){
             n = 0;
