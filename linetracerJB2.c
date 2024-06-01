b4b1d5694c8999ba736d35f8220f5ceb73110b8d
JEEEUNN <lobella2647@gmail.com>
2024-06-02 05:15:08 +0900

    Update linetracerJB2.c

--- linetracerJB2.c
+++ linetracerJB2.c
@@ -123,7 +123,7 @@ int main() {
             Car_Right(200, 200);
             delay(200);
             Car_Left(100, 100);
-            delay(1400);
+            delay(1500);
             Car_Run(40, 40);
             delay(400);
             Car_Right(200, 200);
