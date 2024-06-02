62615e14173cb5d4150f467eaf7d24680ff199b4
jaehoon-L <151628298+jaehoon-L@users.noreply.github.com>
2024-06-02 21:53:47 +0900

    Update racing.c

--- racing.c
+++ racing.c
@@ -240,13 +240,13 @@ int main(int argc, char *argv[]) {
 			fflush(stdout);
             		printf("데이터 송수신 시작\n");
 
-        //서버 통신
+        	//서버 통신
 
-            // 서버로부터 데이터 수신
+            	// 서버로부터 데이터 수신
 			info = global_info;
 			printf("수신\n");
 			fflush(stdout);
-            // 버퍼를 구조체로 복사
+            	// 버퍼를 구조체로 복사
 	        	player_info = info.players[char_num];
             		enemy_info = info.players[enemy_num];
             		int ex;
@@ -259,7 +259,7 @@ int main(int argc, char *argv[]) {
 		        item_state = now_item.status;
 			fflush(stdout);
 		
-		            //bomb
+		        //bomb
 		  	int eleft_x = ex;
 		        int eleft_y = ey-1;
 		        int eup_x = ex-1;
@@ -298,7 +298,7 @@ int main(int argc, char *argv[]) {
 		        	enode_u = info.map[eup_x][eup_y];
 		                eu_item = enode_u.item;
 		            	if(eu_item.status == 0){
-				            eu_item.score =0;
+				        eu_item.score =0;
 			        }
 			        else if(eu_item.status ==2){
 					eu_item.score = -8;
@@ -320,13 +320,15 @@ int main(int argc, char *argv[]) {
 
 		        if(el_item.score>er_item.score){ebest_node = enode_l;}
 		        else{ebest_node = enode_r;}
+			
 		        Item ebest_item = ebest_node.item;
+			
 		        if(eu_item.score>ebest_item.score){ebest_node = enode_u;}
 		        if(ed_item.score>ebest_item.score){ebest_node = enode_d;}
 		    
 		        int efuture_x = ebest_node.row;
 		        int efuture_y = ebest_node.col;
-		            //bomb check
+		        //bomb check
 		
 		            
 		        game_state.row = index_x;
@@ -347,11 +349,11 @@ int main(int argc, char *argv[]) {
 			printf("send complete");
 			fflush(stdout);
         
-        /*
-            algorithm 구현(linetracer)
-        */
+        		/*
+            		algorithm 구현(linetracer)
+        		*/
 
-        // Finding future_x & future_y
+        		// Finding future_x & future_y
 		        int past_x = present_x;
 		        int past_y = present_y;
 		        present_x = index_x;
@@ -382,6 +384,7 @@ int main(int argc, char *argv[]) {
 				}
 			}
 		        else{l_item.score = -100;}
+			
 		        if(right_x>=0 && right_x<=4 && right_y>=0 && right_y<=4){
 		        	node_r = info.map[right_x][right_y];
 		            	r_item = node_r.item;
@@ -393,6 +396,7 @@ int main(int argc, char *argv[]) {
 			        }
 			}
 		        else{r_item.score = -100;}
+			
 		        if(up_x>=0 && up_x<=4 && up_y>=0 && up_y<=4){
 		            	node_u = info.map[up_x][up_y];
 		            	u_item = node_u.item;
@@ -404,6 +408,7 @@ int main(int argc, char *argv[]) {
 			        }
 		        }
 		        else{u_item.score = -100;}
+			
 		        if(down_x>=0 && down_x<=4 && down_y>=0 && down_y<=4){
 		        	node_d = info.map[down_x][down_y];
 		            	d_item = node_d.item;
@@ -420,7 +425,9 @@ int main(int argc, char *argv[]) {
 			
 		        if(l_item.score>r_item.score){best_node = node_l;}
 		        else{best_node = node_r;}
+			
 		        Item best_item = best_node.item;
+			
 		        if(u_item.score>best_item.score){best_node = node_u;}
 		        if(d_item.score>best_item.score){best_node = node_d;}
 		
@@ -428,75 +435,74 @@ int main(int argc, char *argv[]) {
 		        int future_y = best_node.row;
 		
 			printf("x:%d, y:%d \n", future_x, future_y); 
-        // Greedy algorithm
+        		// Greedy algorithm
         
-	fflush(stdout);
+			fflush(stdout);
 
-        int fpp_x = future_x-past_x;
-        int fpp_y = future_y-past_y;
-        int fp_x = future_x-present_x;
-        int fp_y = future_y-present_y;
-        int pp_x = present_x-past_x;
-        int pp_y = present_y-past_y;
+        		int fpp_x = future_x-past_x;
+        		int fpp_y = future_y-past_y;
+		        int fp_x = future_x-present_x;
+		        int fp_y = future_y-present_y;
+        		int pp_x = present_x-past_x;
+        		int pp_y = present_y-past_y;
 
-        if(past_x==-1 && past_y==0){
-            if(present_x==0 && present_y==0){
-                if(fp_x==0 && fp_y==1){run_direct = 'l';}
-                else if(fp_x==1 && fp_y==0){run_direct = 'f';}
-		}
-		}
-	else if(past_x == 5 && past_y == 4){
-            if(present_x==4 && present_y==4){
-                if(fp_x==0 && fp_y==-1){run_direct = 'l';}
-                else if(fp_x==-1 && fp_y==0){run_direct = 'f';}
-            }
-        }
-        else{
-        if(fpp_x==0 && fpp_y==0){run_direct = 'b';}
-        else if(abs(fpp_x)==2 && fpp_y==0){run_direct = 'f';}
-        else if(fpp_x==0 && abs(fpp_y)==2){run_direct = 'f';}
-        else if(pp_x==1 && pp_y==0){
-            if(fp_x==0 && fp_y==-1){run_direct = 'r';}
-            else if(fp_x=0 && fp_y==1){run_direct = 'l';}
-        }
-        else if(pp_x==-1 && pp_y==0){
-            if(fp_x==0 && fp_y==-1){run_direct = 'l';}
-            else if(fp_x==0 && fp_y==1){run_direct = 'r';}
-        }
-        else if(pp_x==0 && pp_y==1){
-            if(fp_x==-1 && fp_y==0){run_direct = 'l';}
-            else if(fp_x==1 && fp_y==0){run_direct = 'r';}
-        }
-        else if(pp_x==0 && pp_y==-1){
-            if(fp_x==-1 && fp_y==0){run_direct = 'r';}
-            else if(fp_x==1 && fp_y==0){run_direct = 'l';}
-        }
-        }
-   }
-        // linetracer
+        		if(past_x==-1 && past_y==0){
+            			if(present_x==0 && present_y==0){
+                			if(fp_x==0 && fp_y==1){run_direct = 'l';}
+                			else if(fp_x==1 && fp_y==0){run_direct = 'f';}
+				}
+			}
+			else if(past_x == 5 && past_y == 4){
+            			if(present_x==4 && present_y==4){
+                			if(fp_x==0 && fp_y==-1){run_direct = 'l';}
+			                else if(fp_x==-1 && fp_y==0){run_direct = 'f';}
+            			}
+        		}
+        		else{
+        			if(fpp_x==0 && fpp_y==0){run_direct = 'b';}
+        			else if(abs(fpp_x)==2 && fpp_y==0){run_direct = 'f';}
+        			else if(fpp_x==0 && abs(fpp_y)==2){run_direct = 'f';}
+        			else if(pp_x==1 && pp_y==0){
+            				if(fp_x==0 && fp_y==-1){run_direct = 'r';}
+            				else if(fp_x=0 && fp_y==1){run_direct = 'l';}
+        			}
+        			else if(pp_x==-1 && pp_y==0){
+            				if(fp_x==0 && fp_y==-1){run_direct = 'l';}
+            				else if(fp_x==0 && fp_y==1){run_direct = 'r';}
+        			}
+        			else if(pp_x==0 && pp_y==1){
+			            	if(fp_x==-1 && fp_y==0){run_direct = 'l';}
+            				else if(fp_x==1 && fp_y==0){run_direct = 'r';}
+			        }
+        			else if(pp_x==0 && pp_y==-1){
+            				if(fp_x==-1 && fp_y==0){run_direct = 'r';}
+            				else if(fp_x==1 && fp_y==0){run_direct = 'l';}
+        			}
+        		}
+   		} //else 끝
+		
+        	// linetracer
+		time_t start_time = time(NULL);
+    		while (difftime(time(NULL), start_time) < 2) {
 
+    			if(run_direct == 'l'){ // left
+				int trackValue1 = digitalRead(TRACKING_PIN1);
+        			int trackValue2 = digitalRead(TRACKING_PIN2);
+        			int trackValue3 = digitalRead(TRACKING_PIN3);
+        			int trackValue4 = digitalRead(TRACKING_PIN4);
 
-	time_t start_time = time(NULL);
-    while (difftime(time(NULL), start_time) < 2) {
-
-    if(run_direct == 'l'){ // left
-	int trackValue1 = digitalRead(TRACKING_PIN1);
-        int trackValue2 = digitalRead(TRACKING_PIN2);
-        int trackValue3 = digitalRead(TRACKING_PIN3);
-        int trackValue4 = digitalRead(TRACKING_PIN4);
-
-        // 트래킹 핀 값을 바탕으로 자동차 제어 로직
-        if (f == 3){
-            Car_Stop();    
-            delay(100);
-            f = 0;
-            l += 1;
-        }
-        if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 1) {
-            n = 0;
-            Car_Run(70, 70);
-        } 
-        else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 1) {
+        			// 트래킹 핀 값을 바탕으로 자동차 제어 로직
+        			if (f == 3){
+            				Car_Stop();    
+            				delay(100);
+				        f = 0;
+            				l += 1;
+        			}	
+        			if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 1) {
+            				n = 0;
+            				Car_Run(70, 70);
+        			} 
+        			else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 1) {
             n = 0;
             Car_Left(200, 200);
             delay(260);
