212734fede57d34793b38b803a0d9e082cbbbdb0
JEEEUNN <lobella2647@gmail.com>
2024-06-03 00:04:00 +0900

    Update linetracerJL1.c

--- linetracerJL1.c
+++ linetracerJL1.c
@@ -120,7 +120,12 @@ int main() {
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 1) {
             n = 0;
-            Car_Run(80, 80);
+            Car_Left(80, 80);
+            delay(50);
+            Car_Run(40, 40);
+            delay(30);
+            Car_Left(80, 80);
+            delay(50);
         }
         else if (trackValue2 == 1 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 1) {
             n = 0;
@@ -158,11 +163,21 @@ int main() {
         //} 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 0) {
             n = 0;
-            Car_Run(80, 80);
+            Car_Left(80, 80);
+            delay(50);
+            Car_Run(40, 40);
+            delay(30);
+            Car_Left(80, 80);
+            delay(50);
         } 
         else if(trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 1){
             n = 0;
-            Car_Run(80, 80);
+            Car_Left(80, 80);
+            delay(50);
+            Car_Run(40, 40);
+            delay(30);
+            Car_Left(80, 80);
+            delay(50);
         }
         else if(trackValue2 == 1 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 0){
             n = 0;
@@ -174,6 +189,15 @@ int main() {
             delay(50);
             Car_Run(40, 40);
             delay(30);
+        }
+        else if (trackValue2 == 1 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 0) {
+            n = 0;
+            Car_Left(80, 80);
+            delay(50);
+            Car_Run(40, 40);
+            delay(30);
+            Car_Left(80, 80);
+            delay(50);
         }
         // else if(trackValue2 == 1 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 1){
         //     n = 0;
