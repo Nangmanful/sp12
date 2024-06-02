04c250190ee80e89ad4fb6bc8efa09e80ea93e00
Nangmanful <154849613+Nangmanful@users.noreply.github.com>
2024-06-03 01:06:34 +0900

    Update linetracer.c

--- linetracer.c
+++ linetracer.c
@@ -117,7 +117,7 @@ int main() {
         if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 1) {
             n = 0;
             Car_Run(80, 80);
-            delay(30);
+            delay(10);
         } 
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 1) {
             n = 0;
@@ -167,7 +167,7 @@ int main() {
         else if (trackValue2 == 0 && trackValue3 == 0 && trackValue1 == 0 && trackValue4 == 0) {
             n = 0;
             Car_Run(80, 80);
-            delay(50);
+            delay(10);
         } 
         else if(trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 1){
             n = 0;
@@ -193,14 +193,14 @@ int main() {
             Car_Back(40,40);
         }
         else if(trackValue2 == 1 && trackValue3 == 1 && trackValue1 == 1 && trackValue4 == 1){
-            if(n == 33){
+            if(n == 55){
                 Car_Back(100, 100);   
                 delay(50);
                 f += 1;
             }
             else{
             Car_Run(80,80);
-            delay(20);
+            delay(10);
             n += 1;
             }
         }
