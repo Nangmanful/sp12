e3263db6e7c4681b2dc23568d581f2929c6a1673
Nangmanful <154849613+Nangmanful@users.noreply.github.com>
2024-06-02 21:43:04 +0900

    Update racing.c

--- racing.c
+++ racing.c
@@ -122,308 +122,312 @@ void* qrRecognitionThread() {
 
 
 int main(int argc, char *argv[]) {
-	printf("시작\0");
-    int present_x;
+	printf("main 시작\n");
+	fflush(stdout);
+	int present_x;
         char run_direct;
 	int present_y;
-    int port = atoi(argv[1]);
-    int char_num = atoi(argv[2]);
-    const char* ip_address = argv[3];
-    int enemy_num;
-    if(char_num == 0){
-        enemy_num = 1;
-	present_x = -1;
-	present_y = 0;
-    }
-    else{
-        enemy_num = 0;
-	present_x = 5;
-	present_y = 4;
-    }
-    int pin = 27;
+	int port = atoi(argv[1]);
+	int char_num = atoi(argv[2]);
+	const char* ip_address = argv[3];
+	int enemy_num;
+    	if(char_num == 0){
+        	enemy_num = 1;
+		present_x = -1;
+		present_y = 0;
+    	}
+    	else{
+        	enemy_num = 0;
+		present_x = 5;
+		present_y = 4;
+    	}
+    	int pin = 27;
     
-    if (wiringPiSetup() == -1) {
-        printf("WiringPi setup failed!\n");
-        return 1;
-    }
-	printf("시작\0");
-    pinMode(pin, INPUT);
-    pullUpDnControl(pin, PUD_UP);
+    	if (wiringPiSetup() == -1) {
+        	printf("WiringPi setup failed!\n");
+        	return 1;
+    	}
+	printf("real 시작\n");
+    	pinMode(pin, INPUT);
+    	pullUpDnControl(pin, PUD_UP);
 
-    pinMode(TRACKING_PIN1, INPUT);
-    pinMode(TRACKING_PIN2, INPUT);
-    pinMode(TRACKING_PIN3, INPUT);
-    pinMode(TRACKING_PIN4, INPUT);
+    	pinMode(TRACKING_PIN1, INPUT);
+    	pinMode(TRACKING_PIN2, INPUT);
+    	pinMode(TRACKING_PIN3, INPUT);
+    	pinMode(TRACKING_PIN4, INPUT);
 
-    i2c_init();
+    	i2c_init();
 
-    int sock;
-    struct sockaddr_in server_addr;
+    	int sock;
+    	struct sockaddr_in server_addr;
 
             // 소켓 생성
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        perror("Could not create socket");
+    	sock = socket(AF_INET, SOCK_STREAM, 0);
+    	if (sock == -1) {
+        	perror("Could not create socket");
                 // return 1;
-    }
+    	}
 
             // 서버 주소 구조체 설정
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(ip_address);
-    server_addr.sin_port = htons(port);
+    	server_addr.sin_family = AF_INET;
+    	server_addr.sin_addr.s_addr = inet_addr(ip_address);
+    	server_addr.sin_port = htons(port);
 
             // 서버에 연결
-    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    	if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
                 perror("Connect failed");
                 // return 1;
-    }
+    	}	
 
-    printf("Connected to server\0");
+    	printf("Connected to server\0");
 	fflush(stdout);
-    pthread_t recv_thread;
-    thread_args *args = malloc(sizeof(thread_args));
-    args->sock = sock;
+    	pthread_t recv_thread;
+    	thread_args *args = malloc(sizeof(thread_args));
+    	args->sock = sock;
 
-    if (pthread_create(&recv_thread, NULL, handle_info, args) != 0) {
-        perror("Could not create thread");
-        close(sock);
-        exit(EXIT_FAILURE);
-    }//server thread
+    	if (pthread_create(&recv_thread, NULL, handle_info, args) != 0) {
+        	perror("Could not create thread");
+        	close(sock);
+        	exit(EXIT_FAILURE);
+    	}//server thread
 	pthread_t threadId;
-    if (pthread_create(&threadId, NULL, qrRecognitionThread, NULL) != 0) {
-        perror("Failed to create QR recognition thread");
-        return 1;
-    } // qr thread
-char* index ="77";
+    	if (pthread_create(&threadId, NULL, qrRecognitionThread, NULL) != 0) {
+        	perror("Failed to create QR recognition thread");
+        	return 1;
+    	} // qr thread
+	char* index ="77";
 	while (1) {
-	    int n = 0;
-    	int f = 0;
-    	int k = 0;
-    	int l = 0;
-        char direct = 'r';
-        int index_x; // our
-        int index_y; // our
-        DGIST info;
-        client_info player_info;
-        client_info enemy_info;
-        Node node;
-        Item now_item;
-        ClientAction game_state;
-        printf("qr시작\n");
-	fflush(stdout);
-        pthread_mutex_lock(&qrCodeMutex);
-	int count = 1;
-	printf("index : %s, qrcode : %s\n", index, qrCodeData);
-	fflush(stdout);
-        if (strcmp(qrCodeData, "77") != 0 && strcmp(index, qrCodeData) != 0) {
-            printf("QR 코드 데이터: %s\n", qrCodeData);
-        	index = qrCodeData;
-		count = 0;
-	}
-	printf("unlock\n");
-	fflush(stdout);
-        pthread_mutex_unlock(&qrCodeMutex);
-	printf("qr끝 \n");
-	fflush(stdout);
-        if (count) { // no qr recognition
-                printf("no qr\0");
+		int n = 0;
+    		int f = 0;
+    		int k = 0;
+    		int l = 0;
+        	char direct = 'r';
+        	int index_x; // our
+        	int index_y; // our
+        	DGIST info;
+        	client_info player_info;
+        	client_info enemy_info;
+        	Node node;
+        	Item now_item;
+		int count = 1;
+        	ClientAction game_state;
+        	printf("qr시작\n");
 		fflush(stdout);
-        }
-        else {
-	        Car_Stop();
-		printf("else start");
+        	pthread_mutex_lock(&qrCodeMutex);
+		printf("index : %s, qrcode : %s\n", index, qrCodeData);
 		fflush(stdout);
-            index_x = index[0] - '0'; // ASCII 값을 실제 숫자로 변환
-            index_y = index[1] - '0'; // ASCII 값을 실제 숫자로 변환
-            printf("%d %d\0", index_x, index_y);
+        	if (strcmp(qrCodeData, "77") != 0 && strcmp(index, qrCodeData) != 0) {
+            		printf("QR 코드 데이터: %s\n", qrCodeData);
+        		index = qrCodeData;
+			count = 0;
+		}
+		printf("unlock\n");
 		fflush(stdout);
-            printf("데이터 송수신 시작\0");
+        	pthread_mutex_unlock(&qrCodeMutex);
+		printf("qr끝 \n");
+		fflush(stdout);
+        	if (count) { // no qr recognition
+                	printf("no qr\n");
+			fflush(stdout);
+        	}
+        	else {
+	        	Car_Stop();
+			printf("else start\n");
+			fflush(stdout);
+            		index_x = index[0] - '0'; // ASCII 값을 실제 숫자로 변환
+            		index_y = index[1] - '0'; // ASCII 값을 실제 숫자로 변환
+            		printf("%d %d\0", index_x, index_y);
+			fflush(stdout);
+            		printf("데이터 송수신 시작\n");
 
         //서버 통신
 
             // 서버로부터 데이터 수신
-		info = global_info;
-		printf("수신\n");
-		fflush(stdout);
+			info = global_info;
+			printf("수신\n");
+			fflush(stdout);
             // 버퍼를 구조체로 복사
-	        player_info = info.players[char_num];
-            enemy_info = info.players[enemy_num];
-            int ex;
-            int ey;
-            ex = enemy_info.col;
-            ey = enemy_info.row;
-            node = info.map[index_x][index_y];
-            now_item = node.item;
-            int item_state;
-            item_state = now_item.status;
-	fflush(stdout);
-
-            //bomb
-            int eleft_x = ex;
-            int eleft_y = ey-1;
-            int eup_x = ex-1;
-            int eup_y = ey;
-            int eright_x = ex;
-            int eright_y = ey+1;
-            int edown_x = ex+1;
-            int edown_y = ey;
-    
-            Node enode_r, enode_l, enode_u, enode_d, ebest_node;
-            Item er_item, el_item, eu_item, ed_item;
-    
-            if(eleft_x>=0 && eleft_x<=4 && eleft_y>=0 && eleft_y<=4){
-                enode_l = info.map[eleft_x][eleft_y];
-                el_item = enode_l.item;
-            if(el_item.status == 0){
-		        el_item.score =0;
-	        }
-	        else if(el_item.status ==2){
-		        el_item.score = -8;
-    	    }
-            }
-            else{el_item.score = -100;}
-            if(eright_x>=0 && eright_x<=4 && eright_y>=0 && eright_y<=4){
-                enode_r = info.map[eright_x][eright_y];
-                er_item = enode_r.item;
-            if(er_item.status == 0){
-		        er_item.score =0;
-	        }
-	        else if(er_item.status ==2){
-		        er_item.score = -8;
-	        }
-            }
-            else{er_item.score = -100;}
-            if(eup_x>=0 && eup_x<=4 && eup_y>=0 && eup_y<=4){
-                enode_u = info.map[eup_x][eup_y];
-                eu_item = enode_u.item;
-            	if(eu_item.status == 0){
-		            eu_item.score =0;
-	            }
-	            else if(eu_item.status ==2){
-		            eu_item.score = -8;
-	            }    
-
-            }
-            else{eu_item.score = -100;}
-            if(edown_x>=0 && edown_x<=4 && edown_y>=0 && edown_y<=4){
-                enode_d = info.map[edown_x][edown_y];
-                ed_item = enode_d.item;
-            	if(ed_item.status == 0){
-	            	ed_item.score =0;
-	            }
-	            else if(ed_item.status ==2){
-		            ed_item.score = -8;
-	            }
-            }
-            else{ed_item.score = -100;}
-    	fflush(stdout);
-
-            if(el_item.score>er_item.score){ebest_node = enode_l;}
-            else{ebest_node = enode_r;}
-            Item ebest_item = ebest_node.item;
-            if(eu_item.score>ebest_item.score){ebest_node = enode_u;}
-            if(ed_item.score>ebest_item.score){ebest_node = enode_d;}
-    
-            int efuture_x = ebest_node.row;
-            int efuture_y = ebest_node.col;
-            //bomb check
-
-            
-            game_state.row = index_x;
-            game_state.col = index_y;
-            if(index_x == efuture_x && index_y == efuture_y){
-                game_state.action = setBomb;
-            }
-            else{
-                game_state.action = move;
+	        	player_info = info.players[char_num];
+            		enemy_info = info.players[enemy_num];
+            		int ex;
+            		int ey;
+           		ex = enemy_info.col;
+            		ey = enemy_info.row;
+            		node = info.map[index_x][index_y];
+		        now_item = node.item;
+		        int item_state;
+		        item_state = now_item.status;
+			fflush(stdout);
+		
+		            //bomb
+		  	int eleft_x = ex;
+		        int eleft_y = ey-1;
+		        int eup_x = ex-1;
+			int eup_y = ey;
+		        int eright_x = ex;
+		        int eright_y = ey+1;
+		        int edown_x = ex+1;
+			int edown_y = ey;
 		    
-            }
-		printf("send time\n");
-		fflush(stdout);
-            if (send(sock, &game_state, sizeof(ClientAction), 0) < 0) {
-                perror("Send failed\0");
-                // return 1;
-            }
-		printf("send complete");
-		fflush(stdout);
+		        Node enode_r, enode_l, enode_u, enode_d, ebest_node;
+		        Item er_item, el_item, eu_item, ed_item;
+		    
+		        if(eleft_x>=0 && eleft_x<=4 && eleft_y>=0 && eleft_y<=4){
+		        	enode_l = info.map[eleft_x][eleft_y];
+		                el_item = enode_l.item;
+		        	if(el_item.status == 0){
+					el_item.score =0;
+			        }
+			        else if(el_item.status ==2){
+				        el_item.score = -8;
+		    	    	}
+		        }
+		        else{el_item.score = -100;}
+		        if(eright_x>=0 && eright_x<=4 && eright_y>=0 && eright_y<=4){
+		                enode_r = info.map[eright_x][eright_y];
+		                er_item = enode_r.item;
+		            	if(er_item.status == 0){
+				        er_item.score =0;
+			        }
+			        else if(er_item.status ==2){
+				        er_item.score = -8;
+			        }
+		        }
+		        else{er_item.score = -100;}
+		        if(eup_x>=0 && eup_x<=4 && eup_y>=0 && eup_y<=4){
+		        	enode_u = info.map[eup_x][eup_y];
+		                eu_item = enode_u.item;
+		            	if(eu_item.status == 0){
+				            eu_item.score =0;
+			        }
+			        else if(eu_item.status ==2){
+					eu_item.score = -8;
+			        }    
+		        }
+		        else{eu_item.score = -100;}
+		        if(edown_x>=0 && edown_x<=4 && edown_y>=0 && edown_y<=4){
+		                enode_d = info.map[edown_x][edown_y];
+		                ed_item = enode_d.item;
+		            	if(ed_item.status == 0){
+			        	ed_item.score =0;
+			        }
+			        else if(ed_item.status ==2){
+				        ed_item.score = -8;
+			        }
+		        }
+		        else{ed_item.score = -100;}
+    			fflush(stdout);
+
+		        if(el_item.score>er_item.score){ebest_node = enode_l;}
+		        else{ebest_node = enode_r;}
+		        Item ebest_item = ebest_node.item;
+		        if(eu_item.score>ebest_item.score){ebest_node = enode_u;}
+		        if(ed_item.score>ebest_item.score){ebest_node = enode_d;}
+		    
+		        int efuture_x = ebest_node.row;
+		        int efuture_y = ebest_node.col;
+		            //bomb check
+		
+		            
+		        game_state.row = index_x;
+		        game_state.col = index_y;
+		        if(index_x == efuture_x && index_y == efuture_y){
+		                game_state.action = setBomb;
+		        }
+		        else{
+		                game_state.action = move;
+				    
+		        }
+			printf("send time\n");
+			fflush(stdout);
+		        if (send(sock, &game_state, sizeof(ClientAction), 0) < 0) {
+		                perror("Send failed\0");
+		                // return 1;
+		        }
+			printf("send complete");
+			fflush(stdout);
         
         /*
             algorithm 구현(linetracer)
         */
 
         // Finding future_x & future_y
-        int past_x = present_x;
-        int past_y = present_y;
-        present_x = index_x;
-        present_y = index_y;
+		        int past_x = present_x;
+		        int past_y = present_y;
+		        present_x = index_x;
+		        present_y = index_y;
+			printf("past x,y : %d, %d\n", past_x, past_y);
+			printf("present x,y : %d, %d\n", present_x, present_y);
+			fflush(stdout);
+ 		        int left_x = present_x;
+		        int left_y = present_y-1;
+		        int up_x = present_x-1;
+		        int up_y = present_y;
+		        int right_x = present_x;
+		        int right_y = present_y+1;
+		        int down_x = present_x+1;
+		        int down_y = present_y;
+		
+		        Node node_r, node_l, node_u, node_d, best_node;
+		        Item r_item, l_item, u_item, d_item;
+		
+		        if(left_x>=0 && left_x<=4 && left_y>=0 && left_y<=4){
+		        	node_l = info.map[left_x][left_y];
+		        	l_item = node_l.item;
+		       		if(l_item.status == 0){
+					l_item.score =0;
+				}	
+				else if(l_item.status ==2){
+					l_item.score = -8;
+				}
+			}
+		        else{l_item.score = -100;}
+		        if(right_x>=0 && right_x<=4 && right_y>=0 && right_y<=4){
+		        	node_r = info.map[right_x][right_y];
+		            	r_item = node_r.item;
+			        if(r_item.status == 0){
+			        	r_item.score =0;
+		        	}
+			        else if(r_item.status ==2){
+				        r_item.score = -8;
+			        }
+			}
+		        else{r_item.score = -100;}
+		        if(up_x>=0 && up_x<=4 && up_y>=0 && up_y<=4){
+		            	node_u = info.map[up_x][up_y];
+		            	u_item = node_u.item;
+			        if(u_item.status == 0){
+				        u_item.score =0;
+			        }
+			        else if(er_item.status ==2){
+				        u_item.score = -8;
+			        }
+		        }
+		        else{u_item.score = -100;}
+		        if(down_x>=0 && down_x<=4 && down_y>=0 && down_y<=4){
+		        	node_d = info.map[down_x][down_y];
+		            	d_item = node_d.item;
+			        if(d_item.status == 0){
+				        d_item.score =0;
+			        }
+			        else if(d_item.status ==2){
+				        d_item.score = -8;
+			        }
+		        }
+		        else{d_item.score = -100;}
 
-        int left_x = present_x;
-        int left_y = present_y-1;
-        int up_x = present_x-1;
-        int up_y = present_y;
-        int right_x = present_x;
-        int right_y = present_y+1;
-        int down_x = present_x+1;
-        int down_y = present_y;
-
-        Node node_r, node_l, node_u, node_d, best_node;
-        Item r_item, l_item, u_item, d_item;
-
-        if(left_x>=0 && left_x<=4 && left_y>=0 && left_y<=4){
-            node_l = info.map[left_x][left_y];
-            l_item = node_l.item;
-       if(l_item.status == 0){
-		l_item.score =0;
-	}
-	else if(l_item.status ==2){
-		l_item.score = -8;
-	}
-	}
-        else{l_item.score = -100;}
-        if(right_x>=0 && right_x<=4 && right_y>=0 && right_y<=4){
-            node_r = info.map[right_x][right_y];
-            r_item = node_r.item;
-	            if(r_item.status == 0){
-		        r_item.score =0;
-	        }
-	        else if(r_item.status ==2){
-		        r_item.score = -8;
-	        }
-	}
-        else{r_item.score = -100;}
-        if(up_x>=0 && up_x<=4 && up_y>=0 && up_y<=4){
-            node_u = info.map[up_x][up_y];
-            u_item = node_u.item;
-	            if(er_item.status == 0){
-		        er_item.score =0;
-	        }
-	        else if(er_item.status ==2){
-		        er_item.score = -8;
-	        }
-            }
-        else{u_item.score = -100;}
-        if(down_x>=0 && down_x<=4 && down_y>=0 && down_y<=4){
-            node_d = info.map[down_x][down_y];
-            d_item = node_d.item;
-	            if(er_item.status == 0){
-		        er_item.score =0;
-	        }
-	        else if(er_item.status ==2){
-		        er_item.score = -8;
-	        }
-            }
-        else{d_item.score = -100;}
-
-        if(l_item.score>r_item.score){best_node = node_l;}
-        else{best_node = node_r;}
-        Item best_item = best_node.item;
-        if(u_item.score>best_item.score){best_node = node_u;}
-        if(d_item.score>best_item.score){best_node = node_d;}
-
-        int future_x = best_node.col;
-        int future_y = best_node.row;
-
-	printf("x:%d, y:%d \n", future_x, future_y); 
+			printf("up, down, left, right : %d, %d, %d, %d\n", u_item.score, d_item.score, l_item.score, r_item.score); 
+			
+		        if(l_item.score>r_item.score){best_node = node_l;}
+		        else{best_node = node_r;}
+		        Item best_item = best_node.item;
+		        if(u_item.score>best_item.score){best_node = node_u;}
+		        if(d_item.score>best_item.score){best_node = node_d;}
+		
+		        int future_x = best_node.col;
+		        int future_y = best_node.row;
+		
+			printf("x:%d, y:%d \n", future_x, future_y); 
         // Greedy algorithm
         
 	fflush(stdout);
