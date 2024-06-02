d1d92b64932303e4fbed6d1ff1018f706befe7e5
jaehoon-L <151628298+jaehoon-L@users.noreply.github.com>
2024-06-02 10:14:17 +0900

    Update qrrecognition.h

--- qrrecognition.h
+++ qrrecognition.h
@@ -1,10 +1,12 @@
 #pragma once
+#include <opencv2/opencv.hpp>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-const char* qrrecognition();
+const char* qrrecognition(cv::VideoCapture* cap);
+
 
 #ifdef __cplusplus
 }
