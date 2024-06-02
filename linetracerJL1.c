7f56b260bb72777e75a1159b59c8d199bfde2f4a
JEEEUNN <lobella2647@gmail.com>
2024-06-03 00:47:01 +0900

    Update linetracerJL1.c

--- linetracerJL1.c
+++ linetracerJL1.c
@@ -127,7 +127,7 @@ int main() {
             Car_Left(200, 200);
             delay(500);
             Car_Run(70, 70);
-            delay(100);
+            delay(300);
         }
         else if (trackValue2 == 1 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 1) {
             n = 0;
@@ -172,7 +172,7 @@ int main() {
             Car_Left(200, 200);
             delay(500);
             Car_Run(70, 70);
-            delay(100);
+            delay(300);
         } 
         else if(trackValue2 == 0 && trackValue3 == 1 && trackValue1 == 0 && trackValue4 == 1){
             n = 0;
@@ -183,7 +183,7 @@ int main() {
             Car_Left(200, 200);
             delay(500);
             Car_Run(70, 70);
-            delay(100);
+            delay(300);
         }
         else if(trackValue2 == 1 && trackValue3 == 0 && trackValue1 == 1 && trackValue4 == 0){
             n = 0;
